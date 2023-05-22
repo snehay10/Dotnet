68a9e67cfb70de6097f1ab13095bccec41203e14
snehay10 <snehasyadav15@gmail.com>
2023-05-22 17:46:19 +0530

    day3

---
+++ repos/list_imp/list_imp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_imp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var  name = new List<string>();
+            name.Add("Sneha");
+            name.Add("Sai");
+            name.Add("Jui");
+            name.Add("Jeevan");
+
+
+
+
+        }
+    }
+}
