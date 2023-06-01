3c35b000459e3c09b133009623c2259eb79b674b
snehay10 <snehasyadav15@gmail.com>
2023-06-01 17:59:30 +0530

    1-6

---
+++ mvc/enum/enum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+   public enum nm
+    {
+        sneha = 0,
+        yadav,
+        sai,
+        a,
+        b,
+        c,
+        d
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            nm n=new nm();
+            
+            Console.WriteLine(n);
+            var a = (nm)1;
+            Console.WriteLine(a);
+            var b= (nm)4;
+            Console.WriteLine(b);
+            
+        }
+    }
+}
